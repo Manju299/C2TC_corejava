c72fad10baee5f18c16d3703647719de39413ea2
user <user@DESKTOP-NKOM4TR>
2022-10-02 22:34:15 +0530

    Multi D Array

---
+++ train/src/com/c2tc/org/multid.java
@@ -0,0 +1,22 @@
+package com.c2tc.org;
+
+import java.util.Scanner;
+
+public class multid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int[][] a = new int [2][2];
+		for (int i=0;i<2;i++) {
+			for (int j=0;j<2;j++) {
+				a[i][j] = sc.nextInt();
+				
+			}
+			
+		}
+
+
+	}
+
+}
